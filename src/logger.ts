f1288b8e08155780c71d6ccb98b8bdb0bec37452
DmitryTevtonsky <tevtonsky1054m1@gmail.com>
2022-12-20 16:02:27 +0300

    Fix.

--- src/logger.ts
+++ src/logger.ts
@@ -17,7 +17,7 @@ const initLogger = () => {
 		],
 	});
 
-	if (process.env.NODE_ENV !== "production") {
+	if (process.env.LOGGER_MODE === "test") {
 		logger.add(new transports.Console({
 			format: format.simple(),
 		}));
