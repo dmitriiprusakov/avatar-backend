a7382bcb4e6043438dc3fd45616be1b51b893a07
DmitryTevtonsky <tevtonsky1054m1@gmail.com>
2023-01-19 15:18:10 +0300

    Cleared new year sale, new prompts added.

--- src/adapters/externals/astria/create-tune.ts
+++ src/adapters/externals/astria/create-tune.ts
@@ -5,57 +5,9 @@ import woman_prompts from "../../../../resources/data/prompts/woman.json";
 import { axiosAstria } from ".";
 import { CreateTuneparams, Tune } from "./types";
 
-const newYearManPrompts = [
-	{
-		"text": "sks handsome man as The King of the North, digital art, ultradetailed, close up, portrait, artstation",
-	},
-	{
-		"text": "sks amazing beautiful man in snowfall, expressive eyes, digital art, ultradetailed, close up, portrait, artstation",
-	},
-	{
-		"text": "sks brutal handsome man from north, viking, in snowfall, portrait",
-	},
-	{
-		"text": "sks man in santa hat and red sweater, close up, portrait",
-	},
-	{
-		"text": "sks man on main poster from film Home Alone, frame from film, film poster",
-	},
-	{
-		"text": "sks man as Grinch from How the Grinch Stole Christmas, film frame",
-	},
-];
-
-const newYearWomanPrompts = [
-	{
-		"text": "sks amazing woman as The Snow Queen, digital art, ultradetailed, close up, portrait, artstation",
-	},
-	{
-		"text": "sks beautiful woman as Elsa of Arendelle, digital art, ultradetailed, close up, portrait, artstation",
-	},
-	{
-		"text": "sks beautiful woman as The Snow Queen, digital art, ultradetailed, close up, portrait, artstation",
-	},
-	{
-		"text": "sks beautiful woman as character Crystal Maiden from game Dota 2, close up, portrait, artstation",
-	},
-	{
-		"text": "sks beautiful woman as Snow Maiden, close up, portrait",
-	},
-	{
-		"text": "sks beautiful woman in snowfall, expressive eyes, digital art, ultradetailed, close up, portrait, artstation",
-	},
-];
-
 const getPromptsForName = (name: Sex) => {
 	if (name === "woman") return woman_prompts;
 	if (name === "man") return man_prompts;
-	return [];
-};
-
-const getAdditionalPromptsForName = (name: Sex) => {
-	if (name === "woman") return newYearWomanPrompts;
-	if (name === "man") return newYearManPrompts;
 	return [];
 };
 
@@ -85,11 +37,7 @@ export const createTune = async ({ logger, chatId, username = "anonymous", name,
 			callback: `${ASTRIA_CALLBACK_DOMAIN}/prompt?i=${chatId}`,
 		}));
 
-		const randomPrompts = getRandom(promptsForName, promptsAmount)
-			.concat(getAdditionalPromptsForName(name)).map(prompt => ({
-				text: prompt.text,
-				callback: `${ASTRIA_CALLBACK_DOMAIN}/prompt?i=${chatId}`,
-			}));
+		const randomPrompts = getRandom(promptsForName, promptsAmount);
 
 		const tune: Tune = {
 			title: username,
