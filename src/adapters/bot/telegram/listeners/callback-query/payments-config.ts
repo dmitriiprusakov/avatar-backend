6297c0284c507816c52193000fd6b53c141d83ee
DmitryTevtonsky <tevtonsky1054m1@gmail.com>
2023-01-25 13:43:20 +0300

    New prices.

--- src/adapters/bot/telegram/listeners/callback-query/payments-config.ts
+++ src/adapters/bot/telegram/listeners/callback-query/payments-config.ts
@@ -19,30 +19,30 @@ type Payment = {
 type Payments = Record<string, Payment>
 export const payments: Payments = {
 	"1": {
-		"title": "Аватарки 80/10",
-		"description": "Оплата 80 аватарок в 10 разных стилях",
-		"text": "80 аватарок в 10 стилях за 299₽",
+		"title": "Аватарки 80",
+		"description": "Оплата 80 аватарок",
+		"text": "80 аватарок за 299₽",
 		"payload": { promptsAmount: 10, payment: 29900 },
 		"prices": [
 			{ "amount": 29900, "label": "Цена" },
 		],
 	},
 	"2": {
-		"title": "Аватарки 160/20",
-		"description": "Оплата 160 аватарок в 20 разных стилях",
-		"text": "160 аватарок в 20 стилях за 599₽",
-		"payload": { promptsAmount: 20, payment: 59900 },
+		"title": "Аватарки 160",
+		"description": "Оплата 160 аватарок",
+		"text": "160 аватарок за 499₽, выгода 20%",
+		"payload": { promptsAmount: 20, payment: 49900 },
 		"prices": [
-			{ "amount": 59900, "label": "Цена" },
+			{ "amount": 49900, "label": "Цена" },
 		],
 	},
 	"3": {
-		"title": "Аватарки 240/30",
-		"description": "Оплата 240 аватарок в 30 разных стилях",
-		"text": "240 аватарок в 30 стилях за 799₽",
-		"payload": { promptsAmount: 30, payment: 79900 },
+		"title": "Аватарки 240",
+		"description": "Оплата 240 аватарок",
+		"text": "240 аватарок за 599₽, выгода 30%",
+		"payload": { promptsAmount: 30, payment: 59900 },
 		"prices": [
-			{ "amount": 79900, "label": "Цена" },
+			{ "amount": 59900, "label": "Цена" },
 		],
 	},
 };
